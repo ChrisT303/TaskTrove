f76d8233b5762946178fe0636e5c715b7f5e2b9d
deftonechris <deftonechris@msn.com>
2024-05-07 14:13:30 -0600

    api fetching, error handling

---
+++ app/drinks/[id]/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SingleDrink = ({params}) => {
+    console.log(params.id);
+  return (
+    <div>SingleDrink</div>
+  )
+}
+
+export default SingleDrink
--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,9 +1,25 @@
-const DrinksPage = () => {
-    return (
-      <div>
-        <h1 className="text-7xl">DrinksPage</h1>
-      </div>
-    );
-  };
-  
-  export default DrinksPage;
+import DrinksList from "@/components/DrinksList";
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+
+
+const fetchDrinks = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch(url);
+    // error handling
+    if(!response.ok){
+        throw new Error('Could not fetch drinks')
+    }
+    const data = await response.json()
+    return data
+}
+
+const DrinksPage = async () => {
+const data = await fetchDrinks()
+  return (
+    <div>
+        <DrinksList drinks={data.drinks}/>
+    </div>
+  );
+};
+
+export default DrinksPage;
